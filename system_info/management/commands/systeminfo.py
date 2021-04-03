eb136fae8f3e7d19e46571ccee746034ac62d3dc
jsandas <jsandas82@gmail.com>
2021-04-03 13:33:27 -0600

    reworked to handle minion not returning

--- system_info/management/commands/systeminfo.py
+++ system_info/management/commands/systeminfo.py
@@ -48,70 +48,67 @@ class Command(BaseCommand):
                     'os','osmajorrelease','osrelease','osversion',
                     'role','selinux','server_category','swap_total'], tgt_type='compound', batch=5)
 
-        try:
-            for host, grains in system_grains.items():
-                # grains will be false if the minion did not respond
-                if grains:
-                    grains['disk_total'] = 0
-                    grains['disk_used'] = 0
-                    grains['disk_free'] = 0
+        for host, grains in system_grains.items():
+            # grains will be false if the minion did not respond
+            if grains:
+                grains['disk_total'] = 0
+                grains['disk_used'] = 0
+                grains['disk_free'] = 0
 
-                    disk_info = client.cmd(host, 'disk.usage', tgt_type='compound')
-                    for _, data in disk_info[host].items():
-                        if data['1K-blocks']:
-                            grains['disk_total'] += int(int(data['1K-blocks']) / 1024)
-                        if data['used']:
-                            grains['disk_used'] += int(int(data['used']) / 1024)
-                        if data['available']:
-                            grains['disk_free'] += int(int(data['available']) / 1024)
-                    hs_info = self._get_highstate(host)
+                disk_info = client.cmd(host, 'disk.usage', tgt_type='compound')
+                for _, data in disk_info[host].items():
+                    if data['1K-blocks']:
+                        grains['disk_total'] += int(int(data['1K-blocks']) / 1024)
+                    if data['used']:
+                        grains['disk_used'] += int(int(data['used']) / 1024)
+                    if data['available']:
+                        grains['disk_free'] += int(int(data['available']) / 1024)
+                hs_info = self._get_highstate(host)
 
-                else:
-                    grains = {}
-                    hs_info = {}
+            else:
+                grains = {}
+                hs_info = {'status': False,
+                            'missing_states': {}
+                }
 
-                results = {'system_info': grains,
-                            'highstate_status': hs_info['status'],
-                            'missing_hs_states': hs_info['missing_states'],
-                            'last_update': timestamp
-                        }
+            results = {'system_info': grains,
+                        'highstate_status': hs_info['status'],
+                        'missing_hs_states': hs_info['missing_states'],
+                        'last_update': timestamp
+                    }
 
-                obj, created = Host_System_Info.objects.update_or_create(name=host, defaults = (results))
-                if created:
-                    self.stdout.write(self.style.SUCCESS('Added new host {} to system_info table'.format(host)))
+            obj, created = Host_System_Info.objects.update_or_create(name=host, defaults = (results))
+            if created:
+                self.stdout.write(self.style.SUCCESS('Added new host {} to system_info table'.format(host)))
 
-                obj.save()
+            obj.save()
 
-            self.stdout.write(self.style.SUCCESS('Updated system_info table'))
-        except Exception as e:
-            self.stdout.write(self.style.WARNING('Failed getting system information: ', e))
-            self.stdout.write(self.style.WARNING('Grain data: {}'.format(system_grains)))
+        self.stdout.write(self.style.SUCCESS('Finished updating system_info table'))
 
 
     def _get_highstate(self, target):
-        ret = {'status': False,
+        ret = {'status': True,
                 'missing_states': {}
             }
+
         client = salt_client()
 
         highstate_return = client.cmd(target, 'state.apply',
                     kwarg={'test': True}, tgt_type='compound')
 
-        try:
-            states = highstate_return[target]
-            status = True
+        states = highstate_return[target]
+
+        if states:
             missing_states = {}
             for name, state in states.items():
                 if not state['result']:
                     self.stdout.write(self.style.WARNING('host {} not compliant with {}'.format(target, name)))
                     status = False
                     missing_states[name] = state['comment']
-
-            ret = {'status': status, 'missing_states': missing_states}
-
-        except Exception as e:
-            #self.stdout.write(self.style.WARNING('Failed getting highstate information: ', e))
-            self.stdout.write(self.style.WARNING('Highstate data: {}'.format(highstate_return)))
-            ret['missing_states'] = {'error': highstate_return[target]}
+            
+            if missing_states:
+                ret = {'status': status, 'missing_states': missing_states}
+        else:
+            ret['status'] = False
 
         return ret
