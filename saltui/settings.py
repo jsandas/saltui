2f8f602687996b4dca87349e3811aabae58ba5ab
jsandas <jsandas82@gmail.com>
2021-04-04 11:13:02 -0600

    start integration oidc

--- saltui/settings.py
+++ saltui/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'pby)@shnci#e-m!4na$3u@1&j055rv(#wa&8#13wb_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost','.digicert.com']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 
 # Application definition
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.postgres',
     'django.contrib.sessions',
     'django.contrib.staticfiles',
+    'mozilla_django_oidc',
     'packages.apps.PackagesConfig',
     'system_info.apps.SystemInfoConfig',
     'users.apps.UsersConfig',
@@ -39,6 +40,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'mozilla_django_oidc.middleware.SessionRefresh',
 ]
 
 ROOT_URLCONF = 'saltui.urls'
@@ -96,8 +98,43 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LOGIN_URL = '/accounts/login'
-#LOGIN_REDIRECT_URL = 'packages/'
+AUTHENTICATION_BACKENDS = [
+    'mozilla_django_oidc.auth.OIDCAuthenticationBackend',
+    'django.contrib.auth.backends.ModelBackend',
+]
+
+LOGIN_URL = '/accounts/login/'
+# LOGIN_REDIRECT_URL = '/'
+
+OIDC_RP_SIGN_ALGO = os.getenv('OIDC_RP_SIGN_ALGO', 'HS256')
+OIDC_OP_JWKS_ENDPOINT = os.getenv('OIDC_OP_JWKS_ENDPOINT')
+OIDC_OP_AUTHORIZATION_ENDPOINT = os.getenv('OIDC_OP_AUTHORIZATION_ENDPOINT')
+OIDC_OP_TOKEN_ENDPOINT = os.getenv('OIDC_OP_TOKEN_ENDPOINT')
+OIDC_OP_USER_ENDPOINT = os.getenv('OIDC_OP_USER_ENDPOINT')
+OIDC_RP_SCOPES = os.getenv('OIDC_RP_SCOPE', 'openid email')
+OIDC_RP_CLIENT_ID = os.getenv('OIDC_RP_CLIENT_ID', '')
+OIDC_RP_CLIENT_SECRET = os.getenv('OIDC_RP_CLIENT_SECRET', '')
+LOGIN_REDIRECT_URL = os.getenv('APP_BASE_DOMAIN', '/')
+# OIDC_REDIRECT_URL = f"{BASE_URL}/admin/oidc/callback/"
+# OIDC_AUTH_REQUEST_EXTRA_PARAMS = {"redirect_uri": OIDC_REDIRECT_URL}
+# LOGOUT_REDIRECT_URL = "<URL path to redirect to after logout>"
+
+# OKTA_AUTH = {
+#     'ORG_URL': 'https://' + os.getenv('OKTA_DOMAIN', '') + '/',
+#     'ISSUER': 'https://' + os.getenv('OKTA_DOMAIN', '') + '/oauth2/default',
+#     'CLIENT_ID': os.getenv('OKTA_CLIENT_ID', ''),
+#     'CLIENT_SECRET': os.getenv('OKTA_CLIENT_SECRET', ''),
+#     'SCOPES': 'openid profile email groups offline_access', # this is the default and can be omitted
+#     'REDIRECT_URI': os.getenv('APP_BASE_DOMAIN', 'http://localhost:8080') + '/accounts/oauth2/callback/',
+#     'LOGIN_REDIRECT_URL': os.getenv('APP_BASE_DOMAIN', '/'),
+#     'CACHE_PREFIX': 'okta', # default
+#     'CACHE_ALIAS': 'default', # default
+#     'PUBLIC_NAMED_URLS': (), # list or tuple of URL names that should be accessible without tokens
+#     'PUBLIC_URLS': (), # list or tuple of URL regular expressions that should be accessible without tokens
+#     'MANAGE_GROUPS': False, # if true the authentication backend will manage django groups for you; include groups in scopes if true
+#     'STAFF_GROUP': 'admin', # members of this group will have the django is_staff user flags set
+#     'SUPERUSER_GROUP': 'admin', # members of this group will have the django is_superuser user flags set.
+# }
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
@@ -119,7 +156,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # application specific settings
